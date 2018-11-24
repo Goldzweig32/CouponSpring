9f64de40e2e843d5153c2f86d1c9e961a6e11bf7
yaniv <yaniv@DESKTOP-LEVA2B6>
2018-11-24 18:56:15 +0200

    New ExceptionsHandler for sprig

--- couponSpringV2/src/main/java/com/yaniv/coupons/exceptions/ExceptionsHandler.java
+++ couponSpringV2/src/main/java/com/yaniv/coupons/exceptions/ExceptionsHandler.java
@@ -4,29 +4,29 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.yaniv.coupons.beans.ErrorBeans;
 
 @ControllerAdvice
 public class ExceptionsHandler implements ExceptionMapper<Throwable> {
+	
+	@ExceptionHandler(ApplicationException.class)
+	public Response toResponse(ApplicationException applicationException) {
 
-	@Override
-	public Response toResponse(Throwable exception) {
-
-		if (exception instanceof ApplicationException) {
-			ApplicationException appException = (ApplicationException) exception;
-
-			int errorCode = appException.getErrorType().getErrorCode();
-			String internalMessage = appException.getMessage();
-			String errorMessage = appException.getErrorType().getErrorMessage();
+			int errorCode = applicationException.getErrorType().getErrorCode();
+			String internalMessage = applicationException.getMessage();
+			String errorMessage = applicationException.getErrorType().getErrorMessage();
 			ErrorBeans errorBeans = new ErrorBeans(errorCode, internalMessage, errorMessage);
 			return Response.status(errorCode).entity(errorBeans).build();
-		}
-
-		String internalMessage = exception.getMessage();
-		ErrorBeans errorBeans = new ErrorBeans(601, internalMessage, "General error");
-		return Response.status(601).entity(errorBeans).build();
 		
 	}
 
+	@Override
+	@ExceptionHandler({Exception.class,Error.class})
+	public Response toResponse(Throwable exception) {
+		String internalMessage = exception.getMessage();
+		ErrorBeans errorBeans = new ErrorBeans(601, internalMessage, "General error");
+		return Response.status(601).entity(errorBeans).build();
+	}
 }
